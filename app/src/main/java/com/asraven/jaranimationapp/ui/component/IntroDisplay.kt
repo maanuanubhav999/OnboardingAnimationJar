44f5dd8f4d1b8b9ca52f2a0991e56ce60823ae65
anubhav <anubhav@sportskeeda.com>
2025-09-26 18:00:40 +0530

    intro display component

---
+++ app/src/main/java/com/asraven/jaranimationapp/ui/component/IntroDisplay.kt
@@ -0,0 +1,69 @@
+package com.asraven.jaranimationapp.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.asraven.jaranimationapp.Intro
+import com.asraven.jaranimationapp.data.remote.ManualBuyEducationData
+
+@Composable
+fun IntroDisplay(
+    modifier: Modifier = Modifier,
+    introData: Intro,
+    onDismiss: () -> Unit
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.8f)) // Example background
+            .clickable { onDismiss() } // Dismiss on click
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        introData.title?.let { title ->
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    lineHeight = 28.sp,
+                    letterSpacing = 0.sp
+                ),
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        introData.subTitle?.let { subtitle ->
+            Text(
+                text = subtitle,
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    lineHeight = 32.sp,
+                    letterSpacing = 0.sp
+                ),
+                color = Color.White.copy(alpha = 0.8f),
+                textAlign = TextAlign.Center
+            )
+        }
+
+    }
+}
