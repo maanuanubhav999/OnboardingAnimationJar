8350a45d9353f1b211c758ae97ad73f6cc1ae094
anubhav <anubhav@sportskeeda.com>
2025-09-27 13:49:25 +0530

    align text to center

--- app/src/main/java/com/asraven/jaranimationapp/ui/component/OnboardingCardExpended.kt
+++ app/src/main/java/com/asraven/jaranimationapp/ui/component/OnboardingCardExpended.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -155,6 +156,7 @@ fun ExpandedCardTitle(
             text = text,
             style = style,
             color = color,
+            textAlign = TextAlign.Center,
             modifier = modifier.sharedElement(
                 rememberSharedContentState(key = "title-$titleKey"),
                 animatedVisibilityScope = animatedVisibilityScope
